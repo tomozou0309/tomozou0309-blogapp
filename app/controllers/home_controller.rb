06069ec0fbda46cdf1790189ab42587dba68b91a
Tomohiro Maruyama <tomozou0309@gmail.com>
2025-07-15 18:30:48 +0900

    '#'→"#"ダブルクオーテーション表示に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-        @title = 'デイトラ'
-        render 'home/index'
+        @title = "デイトラ"
+        render "home/index"
     end
 
     def about
