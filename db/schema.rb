563ec5f095cf312a992a0187ad5e186ee8d71eb0
Tomohiro Maruyama <tomozou0309@gmail.com>
2025-07-15 22:20:55 +0900

    Articleモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_15_091324) do
-  create_table "posts", force: :cascade do |t|
+ActiveRecord::Schema[7.2].define(version: 2025_07_15_125604) do
+  create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
